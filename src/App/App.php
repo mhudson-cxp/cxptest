edc94c7bbe7baaf027f36e4c264202a89abc7b9e
mhudson-cxp <mhudson@connexionpoint.com>
2016-02-03 16:36:38 -0700

    Fixed version.

--- src/App/App.php
+++ src/App/App.php
@@ -7,6 +7,7 @@
  * @author   Mike Hudson <mhudson@connexionpoint.com>
  * @license  http://mhudson-cxp.github.com/ Apache License 2.0
  * @link     http://mhudson-cxp.github.com/
+ * @version $Id$
  */
 namespace App;
 
@@ -18,6 +19,7 @@ namespace App;
  * @author   Mike Hudson <mhudson@connexionpoint.com>
  * @license  http://mhudson-cxp.github.com/ Apache License 2.0
  * @link     http://mhudson-cxp.github.com/
+ * * @version $Id$
  */
 class App
 {
